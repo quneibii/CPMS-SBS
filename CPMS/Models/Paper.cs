2d3d3a133f8d23b39d76baf1e156c3672c6052ca
Brandon Yates <brandonyates@usf.edu>
2021-07-14 00:57:43 -0400

    Edited Paper.cs and its corresponding edit.cshtml and create.cshtml files to have better readability with whitespace and added checkbox functionality.

--- CPMS/Models/Paper.cs
+++ CPMS/Models/Paper.cs
@@ -29,7 +29,7 @@ namespace CPMS.Models
         public int? AuthorId { get; set; }
 
         [Required]
-        public bool? Active { get; set; }//need to change to checkbox
+        public bool Active { get; set; }
 
 
         [StringLength(100)]
@@ -49,137 +49,137 @@ namespace CPMS.Models
 
 
         [StringLength(3)]
-        public string Certification { get; set; }//?
+        public string Certification { get; set; }
 
 
         [Display(Name = "Notes To Reviewers")]
-        public string NotesToReviewers { get; set; }//change to bigger box
+        public string NotesToReviewers { get; set; }
 
 
-        [Display(Name = "Analysis of Algorithms")]//checkbox
-        public bool? AnalysisOfAlgorithms { get; set; }
+        [Display(Name = "Analysis of Algorithms")]
+        public bool AnalysisOfAlgorithms { get; set; }
 
 
-        public bool? Applications { get; set; }//checkbox
+        public bool Applications { get; set; }
 
 
-        public bool? Architecture { get; set; }//checkbox
+        public bool Architecture { get; set; }
 
 
         [Display(Name = "Artifical Intelligence")]
-        public bool? ArtificialIntelligence { get; set; }//checkbox
+        public bool ArtificialIntelligence { get; set; }
 
 
         [Display(Name = "Computer Engineering")]
-        public bool? ComputerEngineering { get; set; }//checkbox
+        public bool ComputerEngineering { get; set; }
 
 
-        public bool? Curriculum { get; set; }//?
+        public bool Curriculum { get; set; }
 
 
-        [Display(Name = "Data Structures")]//checkbox
-        public bool? DataStructures { get; set; }
+        [Display(Name = "Data Structures")]
+        public bool DataStructures { get; set; }
 
 
-        public bool? Databases { get; set; }//checkbox
+        public bool Databases { get; set; }
         
         
         [Display(Name = "Distance Learning")]
-        public bool? DistanceLearning { get; set; }//? CheckBox? is this a subject or saying if you did DL?
+        public bool DistanceLearning { get; set; }
 
 
-        [Display(Name = "Distributed Systems")]//checkbox
-        public bool? DistributedSystems { get; set; }
+        [Display(Name = "Distributed Systems")]
+        public bool DistributedSystems { get; set; }
 
 
-        [Display(Name = "Ethical Societal Issues")]//checkbox
-        public bool? EthicalSocietalIssues { get; set; }
+        [Display(Name = "Ethical Societal Issues")]
+        public bool EthicalSocietalIssues { get; set; }
 
 
-        [Display(Name = "First Year Computing")]//checkbox
-        public bool? FirstYearComputing { get; set; }
+        [Display(Name = "First Year Computing")]
+        public bool FirstYearComputing { get; set; }
         
         
-        [Display(Name = "Gender Issues")]//checkbox
-        public bool? GenderIssues { get; set; }
+        [Display(Name = "Gender Issues")]
+        public bool GenderIssues { get; set; }
 
 
-        [Display(Name = "Grant Writing")]//checkbox
-        public bool? GrantWriting { get; set; }
+        [Display(Name = "Grant Writing")]
+        public bool GrantWriting { get; set; }
 
 
-        [Display(Name = "Graphics Image Processing")]//checkbox
-        public bool? GraphicsImageProcessing { get; set; }
+        [Display(Name = "Graphics Image Processing")]
+        public bool GraphicsImageProcessing { get; set; }
 
 
-        [Display(Name = "Human Computer Interaction")]//checkbox
-        public bool? HumanComputerInteraction { get; set; }
+        [Display(Name = "Human Computer Interaction")]
+        public bool HumanComputerInteraction { get; set; }
 
 
-        [Display(Name = "Labortatory Environments")]//checkbox
-        public bool? LaboratoryEnvironments { get; set; }
+        [Display(Name = "Labortatory Environments")]
+        public bool LaboratoryEnvironments { get; set; }
        
         
-        public bool? Literacy { get; set; }//?
+        public bool Literacy { get; set; }//?
 
 
-        [Display(Name = "Mathmetics In Computing")]//checkbox
-        public bool? MathematicsInComputing { get; set; }
+        [Display(Name = "Mathmetics In Computing")]
+        public bool MathematicsInComputing { get; set; }
 
 
-        public bool? Multimedia { get; set; }//?
+        public bool Multimedia { get; set; }//?
 
 
-        [Display(Name = "Networking Data Communications")]//checkbox
-        public bool? NetworkingDataCommunications { get; set; }
+        [Display(Name = "Networking Data Communications")]
+        public bool NetworkingDataCommunications { get; set; }
 
 
-        [Display(Name = "Non-Major Courses")]//checkbox
-        public bool? NonMajorCourses { get; set; }
+        [Display(Name = "Non-Major Courses")]
+        public bool NonMajorCourses { get; set; }
 
 
-        [Display(Name = "Object Oriented Issues")]//checkbox
-        public bool? ObjectOrientedIssues { get; set; }
+        [Display(Name = "Object Oriented Issues")]
+        public bool ObjectOrientedIssues { get; set; }
 
 
-        [Display(Name = "Operating Systems")]//checkbox
-        public bool? OperatingSystems { get; set; }
+        [Display(Name = "Operating Systems")]
+        public bool OperatingSystems { get; set; }
 
 
-        [Display(Name = "Parallels Processing")]//checkbox
-        public bool? ParallelsProcessing { get; set; }
+        [Display(Name = "Parallels Processing")]
+        public bool ParallelsProcessing { get; set; }
 
 
-        public bool? Pedagogy { get; set; }//checkbox
+        public bool Pedagogy { get; set; }
 
 
-        [Display(Name = "Programming Languages")]//checkbox
-        public bool? ProgrammingLanguages { get; set; }
+        [Display(Name = "Programming Languages")]
+        public bool ProgrammingLanguages { get; set; }
 
 
-        public bool? Research { get; set; }//? Checkbox?
+        public bool Research { get; set; }
 
 
-        public bool? Security { get; set; }//checkbox
+        public bool Security { get; set; }
 
 
-        [Display(Name = "Software Engineering")]//checkbox
-        public bool? SoftwareEngineering { get; set; }
+        [Display(Name = "Software Engineering")]
+        public bool SoftwareEngineering { get; set; }
 
 
-        [Display(Name = "Systems Analysis Design")]//checkbox
-        public bool? SystemsAnalysisAndDesign { get; set; }
+        [Display(Name = "Systems Analysis Design")]
+        public bool SystemsAnalysisAndDesign { get; set; }
 
 
-        [Display(Name = "Using Technology in The Classroom")]//checkbox
-        public bool? UsingTechnologyInTheClassroom { get; set; }
+        [Display(Name = "Using Technology in The Classroom")]
+        public bool UsingTechnologyInTheClassroom { get; set; }
 
 
-        [Display(Name = "Web And Internet Programming")]//checkbox
+        [Display(Name = "Web And Internet Programming")]
         public bool WebAndInternetProgramming { get; set; }
 
 
-        public bool? Other { get; set; }//checkbox
+        public bool Other { get; set; }
         [StringLength(50)]
         [Display(Name = "Other Description")]
         public string OtherDescription { get; set; }
