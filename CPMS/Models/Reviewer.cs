9cf30a3b324531d5cd13350fae01f1a064d39464
Brandon Yates <brandonyates@usf.edu>
2021-07-14 00:10:31 -0400

    Reviewer.cs and Creaste.cshtml (for reviewer.cs) edited to make the appropriate data fields check boxes.

--- CPMS/Models/Reviewer.cs
+++ CPMS/Models/Reviewer.cs
@@ -24,7 +24,7 @@ namespace CPMS.Models
 
 
         [Required]
-        public bool? Active { get; set; }
+        public bool Active { get; set; }
 
 
         [Required]
@@ -101,129 +101,128 @@ namespace CPMS.Models
         [StringLength(5)]
         public string Password { get; set; }
 
-        public bool? AnalysisOfAlgorithms { get; set; }
+        public bool AnalysisOfAlgorithms { get; set; }
 
 
-        public bool? Applications { get; set; }
+        public bool Applications { get; set; }
 
 
-        public bool? Architecture { get; set; }
+        public bool Architecture { get; set; }
 
 
         [Display(Name = "Artifical Intelligence")]
-        public bool? ArtificialIntelligence { get; set; }
+        public bool ArtificialIntelligence { get; set; }
 
 
         [Display(Name = "Computer Engineering")]
-        public bool? ComputerEngineering { get; set; }
+        public bool ComputerEngineering { get; set; }
 
 
-        public bool? Curriculum { get; set; }
+        public bool Curriculum { get; set; }
 
 
         [Display(Name = "Data Structures")]
-        public bool? DataStructures { get; set; }
+        public bool DataStructures { get; set; }
 
 
-        public bool? Databases { get; set; }
+        public bool Databases { get; set; }
 
 
         [Display(Name = "Distanced Learning")]
-        public bool? DistancedLearning { get; set; }
+        public bool DistancedLearning { get; set; }
 
 
         [Display(Name = "Distributed Systems")]
-        public bool? DistributedSystems { get; set; }
+        public bool DistributedSystems { get; set; }
 
 
         [Display(Name = "Ethical Societal Issues")]
-        public bool? EthicalSocietalIssues { get; set; }
+        public bool EthicalSocietalIssues { get; set; }
 
 
         [Display(Name = "First Year Computing")]
-        public bool? FirstYearComputing { get; set; }
+        public bool FirstYearComputing { get; set; }
 
 
         [Display(Name = "Gender Issues")]
-        public bool? GenderIssues { get; set; }
+        public bool GenderIssues { get; set; }
 
 
         [Display(Name = "Grant Writing")]
-        public bool? GrantWriting { get; set; }
+        public bool GrantWriting { get; set; }
 
 
         [Display(Name = "Graphics Image Processing")]
-        public bool? GraphicsImageProcessing { get; set; }
+        public bool GraphicsImageProcessing { get; set; }
 
 
         [Display(Name = "Human Computer Interaction")]
-        public bool? HumanComputerInteraction { get; set; }
+        public bool HumanComputerInteraction { get; set; }
 
 
         [Display(Name = "Labortatory Environments")]
-        public bool? LaboratoryEnvironments { get; set; }
+        public bool LaboratoryEnvironments { get; set; }
 
 
-        public bool? Literacy { get; set; }
+        public bool Literacy { get; set; }
+
         [Display(Name = "Mathmetics In Computing")]
+        public bool MathematicsInComputing { get; set; }
 
 
-        public bool? MathematicsInComputing { get; set; }
-
-
-        public bool? Multimedia { get; set; }
+        public bool Multimedia { get; set; }
 
 
         [Display(Name = "Networking Data Communications")]
-        public bool? NetworkingDataCommunications { get; set; }
+        public bool NetworkingDataCommunications { get; set; }
 
 
         [Display(Name = "Non-Major Courses")]
-        public bool? NonMajorCourses { get; set; }
+        public bool NonMajorCourses { get; set; }
 
 
         [Display(Name = "Object Oriented Issues")]
-        public bool? ObjectOrientedIssues { get; set; }
+        public bool ObjectOrientedIssues { get; set; }
 
 
         [Display(Name = "Operating Systems")]
-        public bool? OperatingSystems { get; set; }
+        public bool OperatingSystems { get; set; }
 
 
         [Display(Name = "Parallel Processing")]
-        public bool? ParallelProcessing { get; set; }
+        public bool ParallelProcessing { get; set; }
 
 
-        public bool? Pedagogy { get; set; }
+        public bool Pedagogy { get; set; }
 
 
         [Display(Name = "Programming Languages")]
-        public bool? ProgrammingLanguages { get; set; }
+        public bool ProgrammingLanguages { get; set; }
 
 
-        public bool? Research { get; set; }
+        public bool Research { get; set; }
 
 
-        public bool? Security { get; set; }
+        public bool Security { get; set; }
 
 
         [Display(Name = "Software Engineering")]
-        public bool? SoftwareEngineering { get; set; }
+        public bool SoftwareEngineering { get; set; }
 
 
         [Display(Name = "Systems Analysis Design")]
-        public bool? SystemsAnalysisAndDesign { get; set; }
+        public bool SystemsAnalysisAndDesign { get; set; }
 
 
         [Display(Name = "Using Technology in The Classroom")]
-        public bool? UsingTechnologyInTheClassroom { get; set; }
+        public bool UsingTechnologyInTheClassroom { get; set; }
 
 
         [Display(Name = "Web And Internet Programming")]
         public bool WebAndInternetProgramming { get; set; }
 
 
-        public bool? Other { get; set; }
+        public bool Other { get; set; }
 
 
         [Display(Name = "Other Description")]
@@ -232,7 +231,7 @@ namespace CPMS.Models
 
 
         [Display(Name = "Reviews Acknowledged")]
-        public bool? ReviewsAcknowledged { get; set; }
+        public bool ReviewsAcknowledged { get; set; }
 
 
         [InverseProperty("Reviewer")]
